8ff41c10af8af628d35f00429fdda26c2879a178
Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
2025-02-06 15:37:21 +0000

    Refactored solution layout, started sample project with tests to test

---
+++ tests/Sample.CalculatorApp/Calculator.cs
@@ -0,0 +1,19 @@
+﻿namespace Sample.CalculatorApp;
+
+public class Calculator
+{
+
+	public static int Sum(params int[] nums)
+	{
+
+		int sum = 0;
+		foreach (int i in nums)
+		{
+			sum += i;
+		}
+
+		return sum;
+
+	}
+
+}
---
+++ tests/Sample.Test.CalculatorApp/VerifySum.cs
@@ -0,0 +1,20 @@
+﻿using Sample.CalculatorApp;
+using TestAccountability;
+
+namespace Sample.Test.CalculatorApp;
+
+public class VerifySum
+{
+
+	[Fact]
+	[Feature("123", FeatureTracker = KnownFeatureTracker.GitHubIssues)]
+	public void AddsTwoNumbers()
+	{
+
+		var addend1 = Random.Shared.Next();
+		var addend2 = Random.Shared.Next();
+
+		Assert.Equal(addend1+addend2, Calculator.Sum(addend1, addend2));
+
+	}
+}
